82d53fba2064dfe8e35a91a64337ee23b2f1b6cc
AnneVi <villette55@gmail.com>
2024-05-10 15:37:26 +0200

    ajout routes 404error

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="error">
+      <h2>404</h2>
+  
+      <img src="@/assets/images/404.jpg" alt="erreur404" />
+    </div>
+  </template>
+  
+  <script setup></script>
+  
+  <style>
+  .error {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .error h2 {
+    font-size: 100px;
+    color: red;
+  }
+  
+  .error img {
+    border: 1px solid black;
+  }
+  </style>
